51c922240090086925da2503baabdb8773709bd3
Dan O'Shea <dan@djoshea.com>
2013-01-11 15:27:21 -0800

    Adding printPValue

---
+++ stats/printPValue.m
@@ -0,0 +1,23 @@
+function printPValue(name, p)
+    fprintf('%20s: ', name);
+
+    sigColor = 'light green';
+    verySigColor = 'light yellow';
+
+    if p < 0.0001
+        tcprintf(verySigColor, '< 0.0001 ****');
+    elseif p < 0.001
+        tcprintf(verySigColor, '< 0.001 ***');
+    elseif p < 0.01
+        tcprintf(sigColor, '< 0.01 **');
+    elseif p < 0.01
+        tcprintf(sigColor, '< 0.01 *');
+    elseif p < 0.05 
+        tcprintf(sigColor, '< 0.05 *');
+    else
+        fprintf('%.3f', p);
+    end
+
+    fprintf('\n');
+
+end
