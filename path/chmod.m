942ce9c389b86640a6848ae80684f182bc807ba2
Dan O'Shea <dan@djoshea.com>
2013-01-14 13:54:29 -0800

    Adding chmod to set permissions

---
+++ path/chmod.m
@@ -0,0 +1,34 @@
+function [status, result] = chmod(permissions, fileList, varargin)
+    % runs chmod permissions fileList
+    p = inputParser;
+    p.addRequired('permissions', @ischar);
+    p.addRequired('fileList', @(x) ischar(x) || iscellstr(x));
+    p.addParamValue('recursive', false, @islogical);
+    p.addParamValue('printError', true, @islogical);
+    p.parse(permissions, fileList, varargin{:});
+    printError = p.Results.printError;
+    recursive = p.Results.recursive;
+
+    if ~iscell(fileList)
+        fileList = {fileList};
+    end
+
+    fileList = cellfun(@GetFullPath, fileList, 'UniformOutput', false);
+    fileListEscaped = cellfun(@escapePathForShell, fileList, 'UniformOutput', false);
+    fileListString = strjoin(fileListEscaped, ' ');
+
+    if recursive
+        flags = '-R ';
+    else
+        flags = '';
+    end
+    cmd = sprintf('chmod %s%s %s', flags, permissions, fileListString);
+
+    [status, result] = system(cmd);
+    
+    if status && printError
+        tcprintf('light red', 'Error running chmod:\n');
+        tcprintf('light red', '%s\n', result);
+    end
+
+end
