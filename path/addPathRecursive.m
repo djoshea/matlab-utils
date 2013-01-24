c6b896435a17d35f111db0ee7031a1b12a29d20d
Dan O'Shea <dan@djoshea.com>
2013-01-24 09:26:58 -0800

    Removing hidden .directories from addPathRecursive

--- path/addPathRecursive.m
+++ path/addPathRecursive.m
@@ -2,24 +2,14 @@ function addPathRecursive(folder)
 % addPathRecursive(folder)
 % Adds a folder and all non-hidden subdirectories to the path
 
-pathstr = genpath(folder);
-addpath(pathstr);
+    pathstr = genpath(folder);
+    
+    % remove hidden folders beginning with .
+    folders = strsplit(pathstr, ':');
+    hiddenDirMask = cellfun(@(x) ~isempty(x), regexp(folders, '/\.[^/]+/*', 'start'));
+    folders = folders(~hiddenDirMask);
+    pathstr = strjoin(folders, ':');
 
-return;
-
-% old inefficient way of doing this via recursion
-% here in case useful
-
-list = dir(folder);
-
-addpath(folder);
-
-for i = 1:length(list)
-	if ~list(i).isdir || strcmp(list(i).name(1), '.')
-		continue;
-	end
-
-	addPathRecursive(fullfile(folder, list(i).name));
-end
+    addpath(pathstr);
 
 end
