12ceb081c58e79fb72ec8615915fe266505d894c
Dan O'Shea <dan@djoshea.com>
2013-02-24 17:46:42 -0800

    Resolve symlink based off readlink, probably need to fix on MacOS

---
+++ path/resolveSymLink.m
@@ -0,0 +1,31 @@
+function target = resolveSymLink(file)
+
+    target = getResolved(file);
+    
+    return;
+
+    if ~exist(file, 'file')
+        % try recursively on its parent
+        [parent leaf ext] = fileparts(file);
+        parent = resolveSymLink(parent);
+        target = fullfile(parent, [leaf ext]);
+    else
+        target = getResolved(file);
+    end
+
+    return;
+    
+    function result = getResolved(file)
+        cmd = sprintf('readlink -m %s', escapePathForShell(GetFullPath(file)));
+        [status result] = system(cmd);
+        if status || isempty(result);
+            fprintf(result);
+            error('Error resolving sym link');
+        end 
+        
+        NEWLINE = 10;
+        if result(end) == NEWLINE
+            result = result(1:end-1);
+        end
+    end
+end
