ddc8df815300192ce32fe036fe651d781c253ca2
Dan O'Shea <dan@djoshea.com>
2014-02-06 13:53:09 -0800

    vertLine fix

--- plotting/vertLine.m
+++ plotting/vertLine.m
@@ -1,7 +1,9 @@
 function h = vertLine(xVal, varargin)
 
+big = 1e10;
+
 h = nan(numel(xVal), 1);
 for i = 1:numel(xVal)
-    h = plot([xVal(i), xVal(i)], [-flintmax, flintmax], ...
+    h = plot([xVal(i), xVal(i)], [-big, big], ...
         'Color', [0.5 0.5 0.5], varargin{:}, 'YLimInclude', 'off');
 end
