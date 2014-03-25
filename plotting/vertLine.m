0b830cdbc45589f1799467fc35b2290dca849b4c
npl.rig5 <npl.rig5@gmail.com>
2014-03-25 16:50:12 -0700

    Correcting vertLine and horzLine legend entries

--- plotting/vertLine.m
+++ plotting/vertLine.m
@@ -4,6 +4,7 @@ big = 1e10;
 
 h = nan(numel(xVal), 1);
 for i = 1:numel(xVal)
-    h = plot([xVal(i), xVal(i)], [-big, big], ...
+    h(i) = plot([xVal(i), xVal(i)], [-big, big], ...
         'Color', [0.5 0.5 0.5], varargin{:}, 'YLimInclude', 'off');
+    set(get(get(h(i), 'Annotation'), 'LegendInformation'), 'IconDisplayStyle', 'off');
 end
