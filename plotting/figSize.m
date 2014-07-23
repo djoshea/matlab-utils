90936f63363fd953fe697f0ea06caa0b16235fc3
Dan O'Shea <dan@djoshea.com>
2014-07-23 10:04:22 -0700

    Fixing figSize

---
+++ plotting/figSize.m
@@ -0,0 +1,31 @@
+function figSize(varargin)
+% figsize(figh, height, width)
+% figsize(height, width) - uses gcf by default
+% 
+% Sizes figure to height x width in cm
+
+if(nargin == 3)
+    figh = varargin{1};
+    height = varargin{2};
+    width = varargin{3};
+elseif(nargin == 2)
+    figh = gcf;
+    height = varargin{1};
+    width = varargin{2};
+else
+    error('Requires 2 or 3 arguments: [figh=gcf], height, width');
+end
+
+set(figh, 'PaperUnits' ,'centimeters');
+set(figh, 'Units', 'centimeters');
+figPos = get(figh,'Position');
+
+set(figh, 'PaperPositionMode', 'auto');
+newPos = [figPos(1), figPos(2), width, height];
+set(figh, 'Position', newPos);
+
+au = AutoAxis.recoverForAxis(gca);
+if ~isempty(au)
+    au.update();
+end
+
