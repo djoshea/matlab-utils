5877dd43c1a8492aea55b19ef9515a0f3c2c3f12
NPL Rig 5 <npl.rig5@gmail.com>
2015-06-12 17:25:17 -0700

    TensorUtils quick additions

--- tensor/TensorUtils.m
+++ tensor/TensorUtils.m
@@ -342,6 +342,11 @@ classdef TensorUtils
             maskByDim = TensorUtils.maskByDimCellSelectAlongDimension(inflatedSize, dims, masks);
             inflated(maskByDim{:}) = maskedTensor;
         end
+        
+        function idxFull = indicesIntoMaskToOriginalIndices(idxIntoMasked, mask)
+            maskInds = find(mask);
+            idxFull = maskInds(idxIntoMasked);
+        end
 
     end
     
@@ -701,6 +706,22 @@ classdef TensorUtils
             if nargout > 1
                 which = cell2mat(makecol(cellfun(@(in, idx) idx*ones(size(in, dim), 1), varargin, ...
                     num2cell(1:numel(varargin)), 'UniformOutput', false)));
+            end
+        end
+        
+        function [out, which] = catWhichIgnoreEmpty(dim, varargin)
+            % works like cat, but returns a vector indicating which of the
+            % inputs each element of out came from
+            
+            isEmpty = cellfun(@isempty, varargin);
+            out = cat(dim, varargin{~isEmpty});
+            if nargout > 1
+                whichMasked = cell2mat(makecol(cellfun(@(in, idx) idx*ones(size(in, dim), 1), varargin(~isEmpty), ...
+                    num2cell(1:nnz(~isEmpty)), 'UniformOutput', false)));
+                
+                % whichMasked indexes into masked varargin, reset these to
+                % index into the original varargin
+                which = TensorUtils.indicesIntoMaskToOriginalIndices(whichMasked, ~isEmpty);
             end
         end
         
