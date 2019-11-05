215faf15676bc25075e02c2be4b3416b27703753
Dan O'Shea <dan@djoshea.com>
2019-11-04 23:38:28 -0800

    Adding isequalbyproperty

---
+++ datatype/isequalbyproperty.m
@@ -0,0 +1,16 @@
+function result = isequalbyproperty(c1, c2)
+
+swarn = warning('off', 'MATLAB:structOnObject');
+
+s1 = struct(c1);
+s2 = struct(c2);
+flds = fieldnames(s1);
+
+for iF = 1:numel(flds)
+    fld = flds{iF};
+    result.(fld) = isequal(s1.(fld), s2.(fld));
+end
+
+warning(swarn);
+
+end
