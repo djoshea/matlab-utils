6ab14aa52063c884d94e5b408020394912d69fcf
Dan O'Shea <dan@djoshea.com>
2020-05-28 17:36:29 -0700

    Improving colorView

--- plotting/colorView.m
+++ plotting/colorView.m
@@ -4,8 +4,13 @@ function h = colorView(map)
 if isa(map, 'function_handle')
     map = map(20);
 end
+
 h = image(permute(map, [3 1 2]));
-axis tight;
-axis off
+ax = gca;
+ax.YTick = [];
+ax.TickDir = 'out';
+box off;
+ax.YRuler.Visible = 'off';
+xlim([0.5 size(map, 1)+0.5]);
 
 end
