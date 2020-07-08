d12fc706eadba3389c5a446ad141b0d4a1c3565e
Dan O'Shea <dan@djoshea.com>
2020-07-08 12:36:46 -0700

    support for struct within nn non-nan counter

--- stats/nn.m
+++ stats/nn.m
@@ -1,7 +1,15 @@
 function n = nn(in, dims)
 
     if nargin < 2
-        n = nnz(isnan(in));
+        if isstruct(in)
+            flds = fieldnames(in);
+            n = 0;
+            for iF = 1:numel(flds)
+                n = n + nn(in.(flds{iF}));
+            end
+        else
+            n = nnz(isnan(in));
+        end
     else
         n = sum(isnan(in), dims);
     end
