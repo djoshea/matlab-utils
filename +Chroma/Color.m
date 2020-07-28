0e1a84a570240c1964e9c151e8e4f5ad8f35aa67
Dan O'Shea <dan@djoshea.com>
2020-07-27 17:22:51 -0700

    Some miscellaneous colormap things

---
+++ +Chroma/Color.m
@@ -0,0 +1,9 @@
+classdef Color
+    
+    properties
+        space string
+        map (:, 4)
+    end
+    
+
+fun
---
+++ colormaps/colorm.m
@@ -0,0 +1,35 @@
+% extracted from linspecer utility
+
+% colorm returns a colormap which is really good for creating informative
+% heatmap style figures.
+% No particular color stands out and it doesn't do too badly for colorblind people either.
+% It works by interpolating the data from the
+% 'spectral' setting on http://colorbrewer2.org/ set to 11 colors
+% It is modified a little to make the brightest yellow a little less bright.
+
+function cmap = colorm(varargin)
+n = 100;
+if ~isempty(varargin)
+    n = varargin{1};
+end
+
+if n==1
+    cmap =  [0.2005    0.5593    0.7380];
+    return;
+end
+if n==2
+     cmap =  [0.2005    0.5593    0.7380;
+              0.9684    0.4799    0.2723];
+          return;
+end
+
+frac=.95; % Slight modification from colorbrewer here to make the yellows in the center just a bit darker
+cmapp = [158, 1, 66; 213, 62, 79; 244, 109, 67; 253, 174, 97; 254, 224, 139; 255*frac, 255*frac, 191*frac; 230, 245, 152; 171, 221, 164; 102, 194, 165; 50, 136, 189; 94, 79, 162];
+x = linspace(1,n,size(cmapp,1));
+xi = 1:n;
+cmap = zeros(n,3);
+for ii=1:3
+    cmap(:,ii) = pchip(x,cmapp(:,ii),xi);
+end
+cmap = flipud(cmap/255);
+end
