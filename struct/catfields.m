1f4b9db19b37206b5e95232266196771637ca9bf
Dan O'Shea <dan@djoshea.com>
2022-08-04 08:41:30 -0700

    Adding struct/catfields

---
+++ struct/catfields.m
@@ -0,0 +1,18 @@
+function vals = catfields(S, args)
+    arguments
+        S (1, 1) struct
+        args.fields (:, 1) string = fieldnames(S);
+        args.asCell (1, 1) logical = true;
+        args.flattenEach (1, 1) logical = true;
+    end
+    % concatenates each field of a struct
+    flds = args.fields;
+
+    vals = cell(numel(flds), 1);
+    for iF = 1:numel(flds)
+        vals{iF} = S.(flds{iF});
+    end
+    if ~args.asCell
+        vals = cat(1, vals{:});
+    end
+end
